c289e75bcf24c0938e32a53c76cbdd5b72dc77ff
Michael Wolf <maw@pobox.com>
2014-10-13 18:26:03 -0500

    threeletter.py

---
+++ threeletter.py
@@ -0,0 +1,22 @@
+
+sqlite_keywords = [
+    'ADD',
+    'ALL',
+    'AND',
+    'AS',
+    'ASC',
+    'BY',
+    'END',
+    'IF',
+    'IN',
+    'IS',
+    'KEY',
+    'NO',
+    'OF',
+    'ON',
+    'OR',
+    'ROW',
+    'SET',
+    'TO',
+]
+
